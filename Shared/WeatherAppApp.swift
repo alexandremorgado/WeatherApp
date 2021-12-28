41a26f66749f0deb52f992da8f46acb8e56b156c
Alexandre Morgado <alexandremorgado@gmail.com>
2021-12-28 11:02:14 -0300

    Initial Commit

---
+++ Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Alexandre Morgado on 28/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
