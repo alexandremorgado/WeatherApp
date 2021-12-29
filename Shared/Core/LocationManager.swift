af9b222c0f56b2810a1c0ad009f18edbd5c8a910
Alexandre Morgado <alexandremorgado@gmail.com>
2021-12-29 13:29:22 -0300

    Add LocationManager class

---
+++ Shared/Core/LocationManager.swift
@@ -0,0 +1,43 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Alexandre Morgado on 29/12/21.
+//
+
+import CoreLocation
+import CoreLocationUI
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var errorOnFetchingLocation: Error?
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        errorOnFetchingLocation = nil
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        errorOnFetchingLocation = error
+        location = nil
+    }
+    
+}
+
+extension CLLocationCoordinate2D: Equatable {}
+
+public func ==(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+    return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+}
