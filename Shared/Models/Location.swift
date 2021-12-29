8187e1661189b85b46cdb3d35b0de7f6cb0e752f
Alexandre Morgado <alexandremorgado@gmail.com>
2021-12-29 09:44:22 -0300

    Add some computed properties and mocks to models

--- Shared/Models/Location.swift
+++ Shared/Models/Location.swift
@@ -6,11 +6,42 @@
 //
 
 import Foundation
+import CoreLocation
  
-// Weather model according to Location API
-struct Location {
+// MARK: - Location model according to MetaWeather API
+
+struct Location: Identifiable, Equatable {
     let woeid: Int
     let title: String
+    let lattLong: String
+    
+    var id: Int { woeid }
 }
 
+// MARK: - Computed properties
+
+extension Location {
+    
+    var coordinate: CLLocationCoordinate2D? {
+        let coordComponents = lattLong.components(separatedBy: ",")
+        guard let latString = coordComponents.first,
+              let longString = coordComponents.last,
+              let lat = Double(latString),
+              let long = Double(longString)
+        else { return nil }
+        return CLLocationCoordinate2D(latitude: lat, longitude: long)
+    }
+    
+}
+
+// MARK: - Codable
+
 extension Location: Decodable { }
+
+// MARK: - Mock
+
+extension Location {
+    static var sample: Location {
+        Location(woeid: 44418, title: "London", lattLong: "51.506321,-0.12714")
+    }
+}
--- Shared/Models/Weather.swift
+++ Shared/Models/Weather.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-// Weather model according to MetaWeather API
+// MARK: - Weather model according to MetaWeather API
+
 struct Weather: Identifiable, Equatable {
     let id: Int
     let applicableDate: Date
@@ -21,8 +22,55 @@ struct Weather: Identifiable, Equatable {
     let predictability: Double
 }
 
+// MARK: - Computed properties
+
+extension Weather {
+    
+    var fahrenheitTemp: Double {
+        (theTemp * 1.8) + 32
+    }
+    
+    var fahrenheitTempText: String {
+        "\(Int(fahrenheitTemp))"// º ℉"
+    }
+    
+    var predictabilityText: String {
+        "\(predictability)"
+    }
+    
+    var iconUrlString: String {
+        "https://www.metaweather.com//static/img/weather/png/64/\(weatherStateAbbr).png"
+    }
+    
+    var iconURL: URL? {
+        URL(string: iconUrlString)
+    }
+    
+}
+
+// MARK: - Codable
+
 extension Weather: Decodable { }
 
 struct LocationWeather: Decodable {
     let consolidatedWeather: [Weather]
+}
+
+// MARK: - Mock
+
+extension Weather {
+    static var sample: Weather {
+        Weather(
+            id: 5367191965794304,
+            applicableDate: Date(),
+            maxTemp: 14.415,
+            minTemp: 12.280000000000001,
+            theTemp: 13.93,
+            weatherStateName: "Heavy Cloud",
+            weatherStateAbbr: "hc",
+            airPressure: 1014.5,
+            humidity: 89,
+            predictability: 71
+        )
+    }
 }
