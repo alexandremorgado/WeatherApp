20b07b5343aa24dce3178e9d7eacfc082a3d85bd
Alexandre Morgado <alexandremorgado@gmail.com>
2021-12-29 12:24:43 -0300

    Improve ViewStatus, allow handle more states

---
+++ Shared/Views/ViewStatus.swift
@@ -0,0 +1,18 @@
+//
+//  ViewStatus.swift
+//  WeatherApp
+//
+//  Created by Alexandre Morgado on 28/12/21.
+//
+
+import Foundation
+
+enum ViewStatus: Equatable {
+    case idle
+    case fetchingLocations
+    case locationsLoaded([Location])
+    case fetchingWeather
+    case weathersLoaded([Weather], location: Location)
+    case empty
+    case error(message: String)
+}
